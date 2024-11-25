11ac029fa3b7feb576597e0ea68ed58a6f38a50d
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-25 17:06:36 -0500

    added FavScreen to tab navigation

--- Screens/EventTabsScreen/EventTabsScreen.jsx
+++ Screens/EventTabsScreen/EventTabsScreen.jsx
@@ -9,6 +9,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { EventsScreen } from '../EventsScreen/EventsScreen';
 import LoadingScreen from '../LoadingScreen/LoadingScreen';
+import { FavsScreen } from '../FavsScreen/FavsScreen';
 
 
 export const EventTabsScreen = ({events, loading, logout, uid}) => {
@@ -23,31 +24,46 @@ export const EventTabsScreen = ({events, loading, logout, uid}) => {
             headerShown: false,
             }}>
                 <Tab.Screen 
-        name="Events"
-        children={() => loading ? <LoadingScreen/> : <EventsScreen uid={uid} events={events}/>}
-        options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons
-              name="calendar-outline"
-              size={size}
-              color={color}
-            />
-          ),
-          headerShown: true,
-          headerTitle: "Events",
-          // includes a button on the left corner to log out from the app
-          headerLeft: ()=>(
-            <Pressable
-                onPressOut={logout}
-            >
-                <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 10,}}>
-                    <Ionicons name='arrow-back'/>
-                    <Text style={{paddingHorizontal: 5,}}>Log Out</Text>
-                </View>
-            </Pressable>
-          )
-        }}
-        />
+                name="Events"
+                children={() => loading ? <LoadingScreen/> : <EventsScreen uid={uid} events={events}/>}
+                options={{
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons
+                    name="calendar"
+                    size={size}
+                    color={color}
+                    />
+                ),
+                headerShown: true,
+                headerTitle: "Events",
+                // includes a button on the left corner to log out from the app
+                headerLeft: ()=>(
+                    <Pressable
+                        onPressOut={logout}
+                    >
+                        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 10,}}>
+                            <Ionicons name='arrow-back'/>
+                            <Text style={{paddingHorizontal: 5,}}>Log Out</Text>
+                        </View>
+                    </Pressable>
+                )
+                }}
+                />
+
+                <Tab.Screen
+                name="Favorites"
+                children={()=> <FavsScreen uid={uid} events={events}/>}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons
+                        name="star"
+                        size={size}
+                        color={color}
+                        />
+                    ),
+                    headerShown: true
+                }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     )
