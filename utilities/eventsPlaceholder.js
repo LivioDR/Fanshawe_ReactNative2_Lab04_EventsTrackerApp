713f7efe45157e992daffa7bcf5c60209dec06a7
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-11-25 15:23:06 -0500

    created mock data placeholder

---
+++ utilities/eventsPlaceholder.js
@@ -0,0 +1,90 @@
+const getRandomId = () => {
+    return Math.random()*10000000
+}
+
+export const eventsPlaceholder = [
+    {
+        "id": getRandomId(),
+        "name": "Unexpected Party at Bag End",
+        "starts": "2024-11-21T12:30:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Trolls Encounter",
+        "starts": "2024-11-22T09:15:00", 
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Battle of Moria",
+        "starts": "2024-11-23T14:45:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Council of Elrond",
+        "starts": "2024-11-24T11:00:00",
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Breaking of the Fellowship",
+        "starts": "2024-11-25T16:30:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Helm's Deep Battle",
+        "starts": "2024-11-26T10:00:00",
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Battle of Pelennor Fields",
+        "starts": "2024-11-27T08:45:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Riddles in the Dark",
+        "starts": "2024-11-28T13:20:00",
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Escape from Goblin Town",
+        "starts": "2024-11-29T15:55:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Battle of the Five Armies",
+        "starts": "2024-11-30T07:30:00",
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Siege of Gondor",
+        "starts": "2024-12-01T12:15:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Flight to the Ford",
+        "starts": "2024-12-02T16:00:00",
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Battle of the Pelennor Fields",
+        "starts": "2024-12-03T09:45:00",
+        "createdBy": "u1t3eZPwcFZiyxJqRzIwcrxmmwX2"
+    },
+    {
+        "id": getRandomId(),
+        "name": "Destruction of the One Ring",
+        "starts": "2024-12-04T11:30:00",
+        "createdBy": "O5aAoLGOhqbaRT4twdpV22YWiM13"
+    }
+]
